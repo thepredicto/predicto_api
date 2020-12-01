4492421e125b2a76934ef1c122ddf41ac227f0cd
Bill Xouris <v.xouris@outlook.com>
2020-12-01 11:05:50 -0800

    Adding alpaca integration code + notebook

---
+++ predicto_api/alpaca_api_wrapper.py
@@ -0,0 +1,284 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime, timedelta
+import sys
+import alpaca_trade_api as tradeapi
+import math
+import time
+
+from predicto_api_wrapper import TradeAction
+
+class AlpacaApiWrapper(object):
+    """Alpaca api wrapper for use with Predicto api wrapper"""
+
+    def __init__(self, apiEndpoint, apiKeyId, apiSecretKey):
+        self.api = tradeapi.REST(apiKeyId, apiSecretKey, apiEndpoint, api_version='v2') 
+        return
+
+    def submit_market_order(self, action, symbol, qty, investmentAmount, startingPrice, targetSellPrice, stopLossPrice, client_order_id):
+        """
+        Call to submit a market order position to alpaca
+        
+        Args:
+            action: TradeAction.BUY or TradeAction.SELL (for shorting)
+            symbol: the ticker to order
+            qty: the quantity of shares to order, if quantity is None or 0, entire investment amount will be used
+            investmentAmount: the max investment amount, needs to be able to satisfy given qty
+            startingPrice: the expected entry price (price might have changed)
+            targetSellPrice: the target exit price (used for trade validations)
+            stopLossPrice: the stop loss price (used for trade validations)
+            client_order_id: a client order id for tracking
+        
+        Returns:
+            Result will be in the form of: (order, newStartingPrice, newTargetPrice, newStopLossPrice, newQuantity) or None if some error occured
+            order: is the alpaca order json, will be none if order couldn't go through
+            newStartingPrice, newTargetPrice, newStopLossPrice, newQuantity are re-adjusted based on latest price
+        """
+
+        # ONLY allow market orders when market is open for now
+        # - this is needed because we can't hedge (submit an oco order) our market order before it is filled!
+        clock = self.api.get_clock()
+        if not clock.is_open :
+            print('Error: submit_market_order only allowed when market is open!')
+            return None
+
+        # ONLY allow opening new position if we are not holding one currently
+        position = self.get_holding_positions(symbol)
+        if position is not None:
+            print('Error: already holding open position')
+            return None
+
+        # ONLY allow opening new position if we haven't traded this symbol (bought or sold) in the last 7 hours (same trading day)
+        sinceDate = str((datetime.utcnow() - timedelta(hours=7)))
+        filledOrders = self.get_filled_orders_for_symbol_since(symbol, sinceDate)
+        if filledOrders is not None and len(filledOrders) > 0:
+            print('Error: bought or sold this symbol in the last 7 hours - This check exists to avoid Day Trading Pattern flag, feel freee to remove.')
+            return None
+
+        # validate prices
+        (goAheadWithTrade, newStartingPrice, newTargetPrice, newStopLossPrice, newQuantity) = self.validate_latest_price_and_stoploss(action, symbol, qty, investmentAmount, startingPrice, targetSellPrice, stopLossPrice)
+        if not goAheadWithTrade:
+            print('Error: validateLatestPriceAndStopLoss failed')
+            return None
+
+        # go ahead and submit order
+        order = self.api.submit_order(
+            side=action.name.lower(),
+            symbol=symbol,
+            type='market',
+            qty=newQuantity,
+            time_in_force='gtc',
+            client_order_id=client_order_id
+        )
+
+        if order is not None:
+            print('Success: submit_market_order ID: {0}'.format(order.id))
+
+        return (order, newStartingPrice, newTargetPrice, newStopLossPrice, newQuantity)
+
+    def open_oco_position(self, parentOrderId, symbol, targetSellPrice, stopLossPrice, client_order_id):
+        """
+        Call to set take-profit / stoploss orders after opening a position with submit_market_order
+        If original position was 'buy', then for this we'll pass 'sell' and vice versa
+        Passed parentOrderId (alpaca market order id) must exist, and be already filled.
+        
+        Args:
+            parentOrderId: the alpaca order id for the market order we want to hedge
+            symbol: the ticker to order
+            targetSellPrice: the target exit price (will be used for oco order)
+            stopLossPrice: the stop loss price (will be used for oco order)
+            client_order_id: a client order id for tracking
+        
+        Returns:
+            The order json returned from alpaca, or None if something went wrong.
+        """
+        parentOrder = self.api.get_order(parentOrderId)
+
+        # validate parentOrder matches and makes sense
+        error = False
+        if parentOrder is None:
+            print('Error: parent order doesnt exist')
+            error = True
+        if parentOrder.symbol != symbol:
+            print('Error: symbols dont match for oco order')
+            error = True
+        if int(parentOrder.filled_qty) == 0:
+            print('Error: parent ordet not yet filled')
+            error = True
+        if parentOrder.status != 'filled':
+            print('Error: parent order status is not open!')
+            error = True
+
+        if error:
+            return None
+
+        newSide = 'sell' if parentOrder.side == 'buy' else 'buy'
+
+        # Check and adjust actual quantity based on held position quantity (saw that EBAY filled_qty = 31, but actual held was 29)
+        openPosition = self.api.get_position(parentOrder.symbol)
+        safeguardQty = parentOrder.filled_qty
+        openPositionQty = abs(int(openPosition.qty))
+        originalOrderQuantity = int(parentOrder.filled_qty)
+        if abs(openPositionQty) != originalOrderQuantity:
+            print('Warning: abs(int(openPosition.qty)) != int(parentOrder.filled_qty) - INVESTIGATE')
+        if abs(openPositionQty) < originalOrderQuantity:
+            safeguardQty = str(abs(openPositionQty))
+
+        order = self.api.submit_order(
+            side=newSide,
+            symbol=parentOrder.symbol,
+            type='limit',
+            qty=safeguardQty,
+            time_in_force='gtc',
+            order_class='oco',
+            take_profit={ 
+                'limit_price': str(targetSellPrice)
+            },
+            stop_loss={
+                'stop_price': str(stopLossPrice)
+            },
+            client_order_id=client_order_id
+        )
+
+        if order is not None:
+            print('Success: open_oco_position ID: {0}'.format(order.id))
+
+        return order
+
+    def validate_latest_price_and_stoploss(self, action, symbol, qty, investmentAmount, startingPrice, targetSellPrice, stopLossPrice):
+        """
+        Check startingPrice is what we expect (might have changed since our recommendation was created)
+        returns (goAheadWithTrade, newStartingPrice, targetSellPrice, stopLossPrice, newQuantity)
+        
+        Args:
+            action: TradeAction.BUY or TradeAction.SELL (for shorting)
+            symbol: the ticker to validate
+            qty: the quantity of shares to validate, if quantity is None or 0, entire investment amount will be used
+            investmentAmount: the max investment amount, needs to be able to satisfy given qty
+            startingPrice: the expected entry price (price might have changed)
+            targetSellPrice: the target exit price (used for trade validations)
+            stopLossPrice: the stop loss price (used for trade validations)
+        
+        Returns:
+            Result will be in the form of:(goAheadWithTrade, newStartingPrice, targetSellPrice, stopLossPrice, newQuantity)
+            goAheadWithTrade: whether given prices make sense and we should go ahead
+            newStartingPrice, newQuantity are re-adjusted based on latest price
+        """
+
+        # double check things look ok
+        if action not in [TradeAction.Buy, TradeAction.Sell]:
+            raise Exception('Wrong trade action passed')
+        if action == TradeAction.Buy and (targetSellPrice <= startingPrice or stopLossPrice >= startingPrice):
+            raise Exception('targetSellPrice must be between stopLossPrice and targetSellPrice')
+        if action == TradeAction.Sell and (targetSellPrice >= startingPrice or stopLossPrice <= startingPrice):
+            raise Exception('targetSellPrice must be between stopLossPrice and targetSellPrice')
+
+        # get latest price 
+        latestPrice = self.get_latest_price(symbol)
+
+        # check if investmentAmount is enough to invest, or calculate qty if not given
+        if investmentAmount is None:
+            raise Exception('investmentAmount is required')
+        if qty is None:
+            qty = math.floor(investmentAmount/latestPrice)
+            if qty == 0:
+                print('Wont process: investmentAmount is less than latest stock Price')
+                return (False, latestPrice, None, None, qty)
+
+        if (qty * latestPrice) > investmentAmount:
+            print('Wont process: (qty * latestPrice) > investmentAmount')
+            return (False, latestPrice, None, None, qty)
+
+        newQuantity = qty
+        newStartingPrice = latestPrice
+
+        # if we already hit our target price, it's too late, cancel order
+        if action == TradeAction.Buy and (latestPrice >= targetSellPrice):
+            print('Wont process: action == TradeAction.Buy and (latestPrice >= targetSellPrice)')
+            return (False, latestPrice, None, None, newQuantity)
+        if action == TradeAction.Sell and (latestPrice <= targetSellPrice):
+            print('Wont process: action == TradeAction.Sell and (latestPrice <= targetSellPrice)')
+            return (False, latestPrice, None, None, newQuantity)
+        if action == TradeAction.Noaction:
+            print('Wont process: action == TradeAction.Noaction')
+            return (False, latestPrice, None, None, newQuantity)
+
+        # otherwise re-adjust StopLossPrice to match original allowed percentage loss
+        stopLossPercent = (stopLossPrice - startingPrice) / startingPrice
+        newStopLossPrice = newStartingPrice + (newStartingPrice * stopLossPercent)
+        # we never re-adjust targetSellPrice for the time being
+        newTargetSellPrice = targetSellPrice
+
+        # if new loss risk is more than potential gain, cancel order
+        newTargetSellPercent = (targetSellPrice - newStartingPrice) / newStartingPrice
+        if abs(newTargetSellPercent) < abs(stopLossPercent):
+            print('Wont process: abs(newTargetSellPercent) < abs(stopLossPercent)')
+            return (False, newStartingPrice, targetSellPrice, newStopLossPrice, newQuantity)
+
+        # do some extra checks here
+        if action == TradeAction.Buy and (newTargetSellPrice <= newStartingPrice or newStopLossPrice >= newStartingPrice):
+            raise Exception('newStartingPrice must be between newStopLossPrice and newTargetSellPrice')
+        if action == TradeAction.Sell and (newTargetSellPrice >= newStartingPrice or newStopLossPrice <= newStartingPrice):
+            raise Exception('newStartingPrice must be between newStopLossPrice and newTargetSellPrice')
+
+        # else go ahead
+        print('Expected price: {0}'.format(startingPrice))
+        print('Actual   price: {0}'.format(latestPrice))
+        print('Original stopLossPrice: {0}'.format(stopLossPrice))
+        print('New stopLossPrice     : {0}'.format(newStopLossPrice))
+
+        return (True, newStartingPrice, targetSellPrice, newStopLossPrice, newQuantity)
+
+    def get_holding_positions(self, symbol=None):
+        """
+        Returns holding positions for given ticker or all holding positions if no ticker given
+        
+        Args:
+            symbol: the ticker to retrieve holding position for. If None, all positions will be retrieved
+        
+        Returns:
+            The position information if symbol is given, or a list of all open positions otherwise
+        """
+        if symbol is not None:
+            # get single position
+            try:
+                return self.api.get_position(symbol)
+            except:
+                return None
+        else:
+            # get all positions
+            positions = self.api.list_positions()
+            for position in positions:
+                print("Holding {} shares of {}".format(position.qty, position.symbol))
+            return positions
+        
+    def get_filled_orders_for_symbol_since(self, symbol, since_date):
+        """
+        Returns orders for given symbol that were filled since given date 
+        
+        Args:
+            symbol: the ticker to orders for
+            since_date: retrieve filled orders since given date (expected format is YYYY-MM-DD)
+        
+        Returns:
+            A list of orders.
+        """
+        orders = self.api.list_orders(status='closed', after=since_date, direction='desc', limit=500)
+
+        return [x for x in orders if x.symbol == symbol and x.status == 'filled']
+
+    def get_latest_price(self, symbol):
+        """
+        Gets latest price of given symbol
+        
+        Args:
+            symbol: the ticker to check
+        
+        Returns:
+            The latest price
+        """
+        barset = self.api.get_barset(symbol, '1Min', limit=5)
+        bars = barset[symbol]
+        latestPrice = bars[-1].c
+
+        return latestPrice
--- predicto_api/predicto_api_wrapper.py
+++ predicto_api/predicto_api_wrapper.py
@@ -1,7 +1,7 @@
-import requests
-import pandas as pd
-import matplotlib.pyplot as plt
 import enum
+import matplotlib.pyplot as plt
+import pandas as pd
+import requests
 
 class TradeAction(enum.IntEnum):
     Noaction = 0
@@ -16,59 +16,51 @@ class PredictoApiWrapper(object):
     def __init__(self, apiSessionId):
         self._head = {'Cookie': 'session={0}'.format(apiSessionId)}
 
-    def get_supported_tickers(self, return_pandas_df=False):
+    def get_supported_tickers(self):
         """Returns a list of supported tickers
         
         Args:
             return_pandas_df: returns a pandas dataframe if True, json otherwise
         
         Returns:
-            A pandas dataframe with all the supported tickers
+            json with all the supported tickers
         """
         endpoint = "{0}/stocks/all".format(PredictoApiWrapper._base_url)
         jsn = requests.get(endpoint, headers=self._head).json()
         
-        return jsn if not return_pandas_df else pd.DataFrame(jsn)
+        return jsn
     
-    def get_forecast(self, ticker, date, return_pandas_df=False):
+    def get_forecast(self, ticker, date):
         """Returns the forecast of the selected ticker for selected date
         
         Args:
             ticker: the ticker of the stock
             date: the date of the forecast (YYYY-MM-DD format)
-            return_pandas_df: returns a pandas dataframe if True, json otherwise
         
         Returns:
-            A pandas dataframe or json with retrieved forecast
+            json with retrieved forecast
         """
         endpoint = "{0}/api/forecasting/{1}/{2}/-1".format(PredictoApiWrapper._base_url, ticker, date)
         jsn = requests.get(endpoint, headers=self._head).json()
         
         forecasting_json = jsn[0]['PredictionsJson']
         
-        if return_pandas_df:
-             return pd.read_json(forecasting_json, orient='index')
-        
         return forecasting_json
     
-    def get_trade_pick(self, ticker, date, return_pandas_df=False):
+    def get_trade_pick(self, ticker, date):
         """Returns generated trade pick of the selected ticker for selected date based on that date's forecast
         
         Args:
             ticker: the ticker of the stock
             date: the date of the forecast (YYYY-MM-DD format)
-            return_pandas_df: returns a pandas dataframe if True, json otherwise
         
         Returns:
-            A pandas dataframe or json with retrieved trade pick
+            json with retrieved trade pick
         """
         endpoint = "{0}/api/forecasting/tradepicks/{1}/{2}/_,0.0,0".format(PredictoApiWrapper._base_url, ticker, date)
         jsn = requests.get(endpoint, headers=self._head).json()
         
         trade_pick_json = jsn['Recommendations'][0]
-        
-        if return_pandas_df:
-            return pd.DataFrame(list(trade_pick_json.items()), columns=['Name', 'Value']).set_index('Name')
         
         return trade_pick_json
             
@@ -85,4 +77,46 @@ class PredictoApiWrapper(object):
         jsn = requests.get(endpoint, headers=self._head).json()
 
         return jsn[0]['ForecastModelGifBlobUrl'].split("?")[0]
+
+    def get_forecast_and_tradepick_info(self, ticker, date, print_it=False):
+        """Helper method to retrieve forecast and trade pick for given ticker and date
         
+        Args:
+            ticker: the ticker of the stock
+            date: the date of the forecast (YYYY-MM-DD format)
+            print_it: whether to print retrieved info and plot
+        
+        Returns:
+            The forecast json and trade pick json as (forecast_json, trade_pick_json)
+        """
+        # retrieve forecast and trade pick
+        forecast_json = self.get_forecast(ticker, date)
+        trade_pick_json = self.get_trade_pick(ticker, date)
+        
+        if print_it:
+            print('Forecast for {0} on {1}'.format(ticker, date))
+
+            # convert to pandas dataframe and plot forecast
+            forecast_df = pd.read_json(forecast_json, orient='index')
+            forecast_df.Prediction.plot()
+            plt.show()
+
+            # print forecast info
+            print(forecast_df[['Prediction', 'Uncertainty']])
+            print()
+
+            # print trade pick info
+            predicted_change_percent = (trade_pick_json['TargetSellPrice'] - trade_pick_json['StartingPrice']) / trade_pick_json['StartingPrice']
+            print('Trade Pick generated based on forecast for {0} on {1}'.format(ticker, date))
+            print('---------------------------------------')
+            print('Action          : \t{0}'.format(TradeAction(trade_pick_json['TradeAction']).name))
+            print('Entry price     : \t{0}'.format(trade_pick_json['StartingPrice']))
+            print('Target price    : \t{0}'.format(trade_pick_json['TargetSellPrice']))
+            print('StopLoss price  : \t{0}'.format(trade_pick_json['TargetStopLossPrice']))
+            print('Expiration Date : \t{0}'.format(trade_pick_json['ExpirationDate']))
+            print('Predicted change: \t{0:.2f} %'.format(predicted_change_percent * 100))
+            print('---------------------------------------')
+            print()
+            print()
+        
+        return (forecast_json, trade_pick_json)
